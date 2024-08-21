ceef340cf34adca07ab3124270fb1dabbc76fc0d
ewrika <egorkaomsk_2003@mail.ru>
2024-08-22 02:51:10 +0600

    Done with vertGridView

--- TestSpar/Views/CardGrid/CardProduct.swift
+++ TestSpar/Views/CardGrid/CardProduct.swift
@@ -2,21 +2,23 @@ import SwiftUI
 
 struct CardProduct: View {
     let product: Product
+    @State private var ChoosenPiece = "Кг"
+    var piece = ["Шт","Кг"]
+    
+    
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             ZStack(alignment: .topLeading) {
                 Rectangle()
                     .foregroundColor(.clear)
-                    .frame(width: 168, height: 168)
                     .background(
                         Image(product.imageName)
                             .resizable()
                             .aspectRatio(contentMode: .fill)
-                            .frame(width: 168, height: 168)
-                            .clipped())
+                            .clipped()
+                    )
 
-                // Верхний левый элемент
                 if let additionalInformation = product.additionalInformation {
                     HStack {
                         Text(additionalInformation)
@@ -25,7 +27,7 @@ struct CardProduct: View {
                             .padding(.trailing, 6)
                             .padding(.top, 2)
                             .padding(.bottom, 4)
-                            .background(Color.HitByPrice)
+                            .background(informationColor(info: additionalInformation))
                             .foregroundColor(.white)
                             .cornerRadius(6)
                         
@@ -33,10 +35,10 @@ struct CardProduct: View {
                     }
                 }
                 
-                // Нижний левый элемент (звезда и рейтинг)
-                VStack {
+                VStack() {
                     Spacer()
-                    HStack {
+                    HStack(spacing:1) {
+                        
                         Image(systemName: "star.fill")
                             .foregroundColor(.yellow)
                             .font(.Regular12)
@@ -46,34 +48,35 @@ struct CardProduct: View {
                             .foregroundColor(.black)
                         
                         Spacer()
+                        
+                        if let discountPercentage = product.discountPercentage {
+                            Text("\(Int(discountPercentage))%")
+                                .font(.ProBold16)
+                                .foregroundColor(.Sale)
+                            
+                        }
+                        
                     }
+                    
                     .padding(.leading, 10)
-                    .padding(.trailing, 0)
-                    .padding(.vertical, 0)
                 }
-                
-                // Верхний правый элемент
+
                 VStack {
                     HStack {
                         Spacer()
-                        
                         Capsule()
                             .fill(Color.white.opacity(0.6))
                             .frame(width: 40, height: 80)
                             .overlay(
                                 VStack {
-                                    Button(action: {
-                                        //
-                                    }) {
+                                    Button(action: {}) {
                                         Image("list")
                                             .frame(width: 16, height: 16)
                                             .foregroundColor(.gray)
                                     }
                                     .padding(.top, 8)
                                     
-                                    Button(action: {
-                                        //
-                                    }) {
+                                    Button(action: {}) {
                                         Image(systemName: "heart")
                                             .font(.system(size: 16))
                                             .foregroundColor(.gray)
@@ -89,48 +92,100 @@ struct CardProduct: View {
                 }
             }
             
-            VStack(alignment: .leading) {
-                //Правый нижний элемент
-                if let discountPercentage = product.discountPercentage {
-                    Text("\(Int(discountPercentage))%")
-                        .font(.ProBold16)
-                        .foregroundColor(.Sale)
-                }
-
+            VStack(alignment: .leading, spacing: 4) {
                 Text(product.name)
                     .font(.Regular12)
                     .foregroundColor(.Price)
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                if let countryFrom = product.countryOfOrigin {
+                    Text(countryFrom)
+                        .font(.Regular12)
+                        .foregroundColor(.Price)
+                }
                 
                 Spacer()
                 
-                HStack {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("\(product.price, specifier: "%.2f")")
-                                .font(.ProBold20)
-                            Image("PerAmountIcon")
-                                .frame(width: 20, height: 20)
-                        }
-                        
-                        if let oldPrice = product.oldPrice {
-                            Text("\(oldPrice, specifier: "%.2f")")
-                                .font(.Regular12)
-                                .foregroundColor(.GrayPrice)
-                                .strikethrough()
+                if product.Choose{
+                    Picker("Кг/Штука",selection: $ChoosenPiece){
+                        ForEach(piece,id: \.self){
+                            Text($0)
                         }
                     }
+                    .pickerStyle(.segmented)
                     
-                    Spacer()
-                    
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 15)
-                            .fill(Color.green)
-                            .frame(width: 48, alignment: .center)
-                            .frame(height: 36, alignment: .center)
-                            .cornerRadius(40)
+                }
+                
+                if product.Variable{
+                    HStack(alignment:.center) {
+                        Button(action: {
+                            // Decrease action
+                        }) {
+                            Image(systemName: "minus")
+                                .foregroundColor(.white)
+                        }
+                        Spacer()
+                        VStack{
+                            
+                            Text("\(String(format: "%.1f", product.weight)) кг")
+                                .font(.ProBold16)
+                                .foregroundColor(.white)
+                                                        
+                            Text("\(String(format:"%.2f",product.price)) ₽")
+                                .font(.ProBold12)
+                                .foregroundColor(.white)
+                            
+                        }
                         
-                        Image("Cart")
-                            .frame(width: 16, height: 16)
+                        Spacer()
+
+                        Button(action: {
+                            // Increase action
+                        }) {
+                            Image(systemName: "plus")
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, minHeight: 36, maxHeight: 36)
+                    .background(Color.green)
+                    .cornerRadius(40)
+                    .padding([.bottom, .top], 5)
+                
+                }
+                
+                
+                if !product.Variable{
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("\(product.price, specifier: "%.1f")")
+                                    .font(.ProBold20)
+                                Image("PerAmountIcon")
+                            }
+                            
+                            if let oldPrice = product.oldPrice {
+                                Text("\(oldPrice, specifier: "%.2f")")
+                                    .font(.Regular12)
+                                    .foregroundColor(.GrayPrice)
+                                    .strikethrough()
+                            }
+                        }
+                        
+                        Spacer()
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.green)
+                                .frame(width: 48, alignment: .center)
+                                .frame(height: 36, alignment: .center)
+                                .cornerRadius(40)
+                            
+                            Image("Cart")
+                                .frame(width: 16, height: 16)
+                        }
                     }
                 }
             }
@@ -142,11 +197,10 @@ struct CardProduct: View {
         }
         .frame(width: 168, height: 278, alignment: .topLeading)
         .cornerRadius(20)
-        .shadow(color: Color(red: 0.55, green: 0.55, blue: 0.55).opacity(0.2),
-                radius: 4, x: 0, y: 0)
+        .shadow(color: Color(red: 0.55, green: 0.55, blue: 0.55).opacity(0.2), radius: 4, x: 0, y: 0)
     }
 }
 
 #Preview {
-    CardProduct(product: mockProducts[0])
+    CardProduct(product: mockProducts[3])
 }
---
+++ TestSpar/Views/CardGrid/VertView.swift
@@ -0,0 +1,32 @@
+//
+//  VertView.swift
+//  TestSpar
+//
+//  Created by Георгий Борисов on 22.08.2024.
+//
+
+import SwiftUI
+
+struct VertView: View {
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        ScrollView{
+            LazyVGrid(columns:columns,spacing: 20){
+                ForEach(mockProducts){ product in
+                    VStack{
+                        CardProduct(product: product)
+                    }
+                }
+            }.padding(.horizontal,20)
+        }
+    }
+}
+
+#Preview {
+    VertView()
+}
