2549b65614935836011b4e5a79eb31ba61f11456
ewrika <egorkaomsk_2003@mail.ru>
2024-08-21 19:30:41 +0600

    Initial Commit

---
+++ TestSpar/TestSparApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSparApp.swift
+//  TestSpar
+//
+//  Created by Георгий Борисов on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
