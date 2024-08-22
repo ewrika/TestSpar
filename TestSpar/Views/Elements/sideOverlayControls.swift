54db6bf56bf787f98e52fc11a7c6b3476477684e
ewrika <egorkaomsk_2003@mail.ru>
2024-08-22 18:18:26 +0600

    Done decomposition

---
+++ TestSpar/Views/Elements/sideOverlayControls.swift
@@ -0,0 +1,46 @@
+//
+//  sideOverlayControls.swift
+//  TestSpar
+//
+//  Created by Георгий Борисов on 22.08.2024.
+//
+
+import SwiftUI
+
+struct sideOverlayControls: View {
+    var body: some View {
+        VStack {
+                   HStack {
+                       Spacer()
+                       Capsule()
+                           .fill(Color.white.opacity(0.6))
+                           .frame(width: 40, height: 80)
+                           .overlay(
+                               VStack {
+                                   Button(action: {}) {
+                                       Image("list")
+                                           .frame(width: 16, height: 16)
+                                           .foregroundColor(.gray)
+                                   }
+                                   .padding(.top, 8)
+                                   
+                                   Button(action: {}) {
+                                       Image(systemName: "heart")
+                                           .font(.system(size: 16))
+                                           .foregroundColor(.gray)
+                                   }
+                                   .padding(.top, 8)
+                               }
+                           )
+                   }
+                   .padding(.trailing, 0)
+                   .padding(.top, 0)
+                   
+                   Spacer()
+               }
+    }
+}
+
+#Preview {
+    sideOverlayControls()
+}
